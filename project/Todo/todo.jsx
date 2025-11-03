4e4ee7de535034a77725da23938acbc554af76f6
Wela coder <ahmaddogar818@gmail.com>
2025-11-03 14:47:06 +0500

    Add files via upload

---
+++ project/Todo/todo.jsx
@@ -0,0 +1,79 @@
+import "./todo.css";
+import {useState} from 'react';
+import { TodoList } from './TodoList';
+import { TodoDate } from './TodoDate';
+import { TodoForm } from "./TodoForm.jsx";
+import { getLocalStorageTodoData, setLocalStorageTodoData } from './todoLocalStorage.jsx';
+
+export const Todo = () => {
+
+    const [task, setTask] = useState(() => getLocalStorageTodoData());
+  
+    const handleFormSubmit = (inputValue) => {
+        const {id, content, checked} = inputValue;
+
+        // to check if the input field is empty or not
+        if(!content) return;
+
+        //* to check if the data is already exixting or not
+        // if(task.includes(inputValue)) return;
+        const ifTodoContentMatched = task.find((curTask) => curTask.content === content);
+        if(ifTodoContentMatched) return;
+
+        setTask((prevTask) => [...prevTask, {id, content, checked}]);
+    };
+
+    //todo add data to localStorage
+     setLocalStorageTodoData(task);
+   
+    //todo handleDeleteTodo function
+    const handleDeleteTodo = (value) => {
+        const updatedTask = task.filter((curTask) => curTask.content !== value);
+        setTask(updatedTask);
+    };
+
+    //todo andleClearTodoButton function
+    const handleClearTodoButton = () => {
+        setTask([]);
+    };
+
+    //todo handleCheckedTodo function
+    const handleCheckedTodo = (content) => {
+        const updatedTask = task.map((curTask) => {
+              if(curTask.content === content)  {
+                return {...curTask, checked: !curTask.checked};
+              }else {
+                return curTask;
+              }
+        });
+        setTask(updatedTask);
+    };
+
+    return(
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+                < TodoDate />
+            </header>
+
+           <TodoForm onAddTodo={handleFormSubmit}/>
+
+            <section className="myUnOrdList">
+                <ul>
+                    {task.map((curTask) => {
+                        return < TodoList 
+                        key={curTask.id}
+                        data={curTask.content}
+                        checked={curTask.checked}
+                        onHandleDeleteTodo= {handleDeleteTodo}
+                        onHandleCheckedTodo= {handleCheckedTodo}
+                        />;
+                    } )}
+                </ul>
+            </section>
+            <section>
+                <button className="clear-btn" onClick={handleClearTodoButton}>Clear All</button>
+            </section>
+        </section>
+    );
+}
---
+++ project/Todo/todoLocalStorage.jsx
@@ -0,0 +1,11 @@
+const todoKey = "reactTodo";
+
+export const getLocalStorageTodoData = () => {
+         const rawTodos = localStorage.getItem(todoKey);
+        return rawTodos ? JSON.parse(rawTodos) : [];
+};
+
+
+export const setLocalStorageTodoData = (task) => {
+         localStorage.setItem(todoKey, JSON.stringify(task));
+};
